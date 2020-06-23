2cd21e568ebf8c2ce04763c92dfb52f957e0e011
Jakhongir Tashpulatov <tashpjak@fel.cvut.cz>
2020-06-23 15:14:57 +0200

    Edited MONGO URL

--- src/example.conf.ts
+++ src/example.conf.ts
@@ -1,5 +1,5 @@
 export const PORT = 8080;
-export const MONGO = 'mongodb://localhost:27017/open-calendar'
+export const MONGO = 'mongodb://mongo:27017/open-calendar'
 export const CLIENT_ID = ''
 export const CLIENT_SECRET = ''
 export const DOMAIN = 'http://localhost:8080'
